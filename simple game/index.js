8fbbcd94e56066f325401d5eaceec6e23d8737a7
Jasperthepark <Jasperhung888@gmail.com>
2025-03-10 07:30:44 +0800

    CHANGED

--- simple game/index.js
+++ simple game/index.js
@@ -354,6 +354,7 @@ function animate(){
         
 
         //asteroids trash can
+        //____
     if(asteroid.position.x + asteroid.radius < 0 || asteroid.position.x - asteroid.radius > canvas.width || asteroid.position.y - asteroid.radius > canvas.height || asteroid.position.y + asteroid.radius < 0){
         asteroids.splice(i, 1)
     }
